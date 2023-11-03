69112d22d8d610e0ada4b36d41b54f116babb726
Ibrahim Abdulbaqi <abdulbaqii403@gmail.com>
2023-11-03 10:51:45 +0100

    Building a calculator

---
+++ script.js
@@ -0,0 +1,43 @@
+    
+    function displayOne() {
+        document.getElementById("demo").innerHTML = 1;}
+        function displayTwo() {
+        document.getElementById('demo').innerHTML = 2;
+        }
+        function displayThr() {
+        document.getElementById('demo').innerHTML = 3;
+        }
+        function displayFou() {
+        document.getElementById('demo').innerHTML = 4;
+        }
+        function displayFiv() {
+        document.getElementById('demo').innerHTML = 5;
+        }
+        function displaySix() {
+        document.getElementById('demo').innerHTML = 6;
+        }
+        function displaySev() {
+        document.getElementById('demo').innerHTML = 7;
+        }
+        function displayEig() {
+        document.getElementById('demo').innerHTML = 8;
+        }
+        function displayNin() {
+        document.getElementById('demo').innerHTML = 9;
+        }
+        function displayZer() {
+        document.getElementById('demo').innerHTML = 0;
+        }
+        function displayDot() {
+        document.getElementById('demo').innerHTML = ".";
+        }
+        function displayPlu() {
+        document.getElementById('demo').innerHTML = "+";
+        }
+        function displayMin() {
+        document.getElementById('demo').innerHTML = "-";
+        }
+        function displayMul() {
+        document.getElementById('demo').innerHTML = "*";
+        }
+        
